f68350b0d7746733243d63c4dd24ef44d78905c1
xiuliang <xliang34@gwu.edu>
2020-07-08 16:25:42 +0800

    the modified one

---
+++ src/main/java/com/huron/springboot_redis/Person.java
@@ -0,0 +1,14 @@
+package com.huron.springboot_redis;
+
+public class Person {
+    int age;
+    int gender;
+    int height;
+
+
+    public Person(int age, int gender, int height) {
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+    }
+}
